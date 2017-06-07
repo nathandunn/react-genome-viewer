ec6f8f277eb509646ed5236d9c1bcb0ff64f08aa
Nathan Dunn <ndunn@me.com>
2017-06-07 12:06:32 -0700

    doing basic svg rendering from reactjs

--- src/GenomeViewer.js
+++ src/GenomeViewer.js
@@ -1,9 +1,20 @@
-var React = require('react');
+import React, {Component} from 'react';
 
-var GenomeViewer = React.createClass({
-	render () {
-		return <div>GenomeViewer</div>;
+class GenomeViewer extends Component {
+	render() {
+
+		return (
+			React.createElement('div', null,
+				React.createElement('svg', {
+						version: '1.1', id: 'lion', x: '0px', y: '0px',
+						viewBox: '0 0 800 800', 'enable-background': 'new 0 0 800 800'
+					},
+					React.createElement('circle', { cx:50,cy:50,r:50, fill:'red',stroke:'black',strokWidth:'3' },null ),
+					React.createElement('circle', { cx:150,cy:50,r:50, fill:'red',stroke:'black',strokWidth:'3' },null )
+				)
+			)
+		);
 	}
-});
+}
 
 export default GenomeViewer;
